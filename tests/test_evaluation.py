32515fa25796b73dfcded3690232a685c8bf9c0c
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2023-04-26 12:35:40 -0400

    test: add unit tests for `evaluation.py`

---
+++ tests/test_evaluation.py
@@ -0,0 +1,67 @@
+import chess
+
+from osmanthus.evaluate import check_endgame
+from osmanthus.evaluate import evaluate_board
+from osmanthus.evaluate import is_favorable_move
+
+def test_favorable_move() -> None:
+    """
+    Test the is_favorable_move function with various board states and moves.
+    Ensure that the function always finds a good move. A move is "favorable"
+    if it leads to a clear advantage for the current player.
+    """
+
+    # Open the positions file
+    with open("tests/test_files/positions.fen") as f:
+        # Read each line (position) in the file and remove trailing whitespace
+        positions = (line.strip() for line in f.readlines())
+
+    # Test each position
+    for fen in positions:
+        # Create a new chess board using the FEN string
+        board = chess.Board(fen)
+        # Check if any of the legal moves are favorable
+        assert any(is_favorable_move(board, move) for move in board.legal_moves)
+
+
+def test_is_endgame() -> None:
+    """
+    Test the check_endgame function with various board states.
+    Ensure that the function correctly identifies endgame states.
+    """
+
+    # Test the starting position (not an endgame)
+    board = chess.Board(chess.STARTING_FEN)
+    assert check_endgame(board) == False
+
+    # Open the endgame file
+    with open("tests/test_files/endgame.fen") as f:
+        # Read each line (position) in the file and remove trailing whitespace
+        endgame = (line.strip() for line in f.readlines())
+
+    # Test each endgame position
+    for fen in endgame:
+        # Create a new chess board using the FEN string
+        board = chess.Board(fen)
+        # Check if the board is in an endgame state
+        assert check_endgame(chess.Board(fen)) == True
+
+
+def test_evaluate_board() -> None:
+    """
+    Test the evaluate_board function with various board states.
+    Ensure that the function correctly evaluates the board position.
+    """
+
+    # Test a position where white is down one pawn
+    white_down_one_pawn = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPP1/RNBQKBNR w KQkq - 0 1")
+    assert evaluate_board(white_down_one_pawn) < 0
+
+    # Test a position where white has played e4
+    white_played_e4 = chess.Board("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1")
+    assert evaluate_board(white_played_e4) > 0
+
+    # Note: The following test case is commented out because it is not needed for the current test suite
+    # Test a position where black has played Nc3
+    # black_played_Nc3 = chess.Board("r1bqkbnr/pppppppp/2n5/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 1 2")
+    # assert evaluate_board(black_played_Nc3) > 0
